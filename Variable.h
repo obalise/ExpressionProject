49acd60af8e733af3520a4213d14198b1f260bb1
Peretti <yoni.peretti@gmail.com>
2021-10-23 18:04:24 +0200

    Attribution d'une valeur à une variable

---
+++ Variable.h
@@ -0,0 +1,44 @@
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+#include <iostream>
+#include <map>
+
+#include "Expression.h"
+
+using namespace std;
+
+class Variable : public Expression
+{
+public:
+    Variable(char _variable);
+    virtual ~Variable();
+    Variable(const Variable& other);
+
+    //void afficher() override;
+    void afficherNPI(ostream &os) const override;
+    float calculer() override;
+    void afficher(ostream &os) const override;
+    float attribution();
+
+    float GetVariable()
+    {
+        return _variable;
+    }
+    void SetVariable(char val)
+    {
+        _variable = val;
+    }
+
+protected:
+
+private:
+
+    char _variable;
+    float _valeur;
+    map <char, float> _symbole;
+};
+
+ostream &operator<<(ostream &os, const Variable& c1);
+
+#endif // VARIABLE_H
