cdee7a66b978b1f4ddefc4d2d19d6952ee8e3959
Florent VILLENEUVE <florentvilleneuve1@gmail.com>
2021-11-17 19:29:57 +0100

    Suite séparation Saisie Expression et Afficher Graphe

--- saisieexpressiongraphe.cpp
+++ saisieexpressiongraphe.cpp
@@ -1,14 +1,239 @@
 #include "saisieexpressiongraphe.h"
 #include "ui_saisieexpressiongraphe.h"
 
+
 SaisieExpressionGraphe::SaisieExpressionGraphe(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::SaisieExpressionGraphe)
 {
     ui->setupUi(this);
+    ui->AffichageNPI->setText("Ici s'affichera l'expression en NPI");
+    ui->AffichageNC->setText("Ici s'affichera l'expression en NC");
 }
 
 SaisieExpressionGraphe::~SaisieExpressionGraphe()
 {
     delete ui;
 }
+
+void SaisieExpressionGraphe::on_Bouton_Constante_clicked()
+{
+    Expression* pConstante = new Constante(_nombreConstanteSaisie);
+    pileExpression.push(pConstante);
+
+    QString valeurEcran = ui->AffichageNPI->text();
+    QString valeurAjout = QString::number(_nombreConstanteSaisie);
+
+    if((valeurEcran == "Ici s'affichera l'expression en NPI")){
+       ui->AffichageNPI->setText(valeurAjout);
+    } else{
+       ui->AffichageNPI->setText(valeurEcran + " " + valeurAjout);
+    }
+
+    Expression* expression = pileExpression.top();
+
+    std::stringstream chaine;
+    chaine << *expression;
+    std::string s = chaine.str();
+    QString qstr = QString::fromStdString(s);
+
+    QString valeurAjout2 = qstr;
+
+    ui->AffichageNC->setText(valeurAjout2);
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Variable_clicked()
+{
+    Expression *pVariable = new Variable('y');
+    pileExpression.push(pVariable);
+
+    QString valeurEcran = ui->AffichageNPI->text();
+    QString valeurAjout = "y";
+
+    if((valeurEcran == "Ici s'affichera l'expression en NPI")){
+       ui->AffichageNPI->setText(valeurAjout);
+    } else{
+       ui->AffichageNPI->setText(valeurEcran + " " + valeurAjout);
+    }
+
+    Expression* expression = pileExpression.top();
+
+    std::stringstream chaine;
+    chaine << *expression;
+    std::string s = chaine.str();
+    QString qstr = QString::fromStdString(s);
+
+    QString valeurAjout2 = qstr;
+
+    ui->AffichageNC->setText(valeurAjout2);
+}
+
+
+
+
+void SaisieExpressionGraphe::on_Bouton_Addition_clicked()
+{
+    Expression *pOperande2 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pOperande1 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pAddition = new Addition(pOperande1, pOperande2);
+
+    pileExpression.push(pAddition);
+
+    QString valeurEcran = ui->AffichageNPI->text();
+    QString valeurAjout = "+";
+
+    if((valeurEcran == "Ici s'affichera l'expression en NPI")){
+       ui->AffichageNPI->setText(valeurAjout);
+    } else{
+       ui->AffichageNPI->setText(valeurEcran + " " + valeurAjout);
+    }
+
+    Expression* expression = pileExpression.top();
+
+    std::stringstream chaine;
+    chaine << *expression;
+    std::string s = chaine.str();
+    QString qstr = QString::fromStdString(s);
+
+    QString valeurAjout2 = qstr;
+
+    ui->AffichageNC->setText(valeurAjout2);
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Multiplication_clicked()
+{
+    Expression *pOperande2 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pOperande1 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pMultiplication = new Multiplication(pOperande1, pOperande2);
+
+    pileExpression.push(pMultiplication);
+
+    QString valeurEcran = ui->AffichageNPI->text();
+    QString valeurAjout = "*";
+
+    if((valeurEcran == "Ici s'affichera l'expression en NPI")){
+       ui->AffichageNPI->setText(valeurAjout);
+    } else{
+       ui->AffichageNPI->setText(valeurEcran + " " + valeurAjout);
+    }
+
+    Expression* expression = pileExpression.top();
+
+    std::stringstream chaine;
+    chaine << *expression;
+    std::string s = chaine.str();
+    QString qstr = QString::fromStdString(s);
+
+    QString valeurAjout2 = qstr;
+
+    ui->AffichageNC->setText(valeurAjout2);
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Puissance_clicked()
+{
+
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Soustraction_clicked()
+{
+    Expression *pOperande2 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pOperande1 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pSoustraction = new Soustraction(pOperande1, pOperande2);
+
+    pileExpression.push(pSoustraction);
+
+    QString valeurEcran = ui->AffichageNPI->text();
+    QString valeurAjout = "-";
+
+    if((valeurEcran == "Ici s'affichera l'expression en NPI")){
+       ui->AffichageNPI->setText(valeurAjout);
+    } else{
+       ui->AffichageNPI->setText(valeurEcran + " " + valeurAjout);
+    }
+
+    Expression* expression = pileExpression.top();
+
+    std::stringstream chaine;
+    chaine << *expression;
+    std::string s = chaine.str();
+    QString qstr = QString::fromStdString(s);
+
+    QString valeurAjout2 = qstr;
+
+    ui->AffichageNC->setText(valeurAjout2);
+}
+
+
+
+void SaisieExpressionGraphe::on_Bouton_Inverse_clicked()
+{
+
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Division_clicked()
+{
+    Expression *pOperande2 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pOperande1 = pileExpression.top();
+    pileExpression.pop();
+    Expression *pDivision = new Division(pOperande1, pOperande2);
+
+    pileExpression.push(pDivision);
+
+    QString valeurEcran = ui->AffichageNPI->text();
+    QString valeurAjout = "/";
+
+    if((valeurEcran == "Ici s'affichera l'expression en NPI")){
+       ui->AffichageNPI->setText(valeurAjout);
+    } else{
+       ui->AffichageNPI->setText(valeurEcran + " " + valeurAjout);
+    }
+
+
+    Expression* expression = pileExpression.top();
+
+    std::stringstream chaine;
+    chaine << *expression;
+    std::string s = chaine.str();
+    QString qstr = QString::fromStdString(s);
+
+    QString valeurAjout2 = qstr;
+
+    ui->AffichageNC->setText(valeurAjout2);
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_ValAbs_clicked()
+{
+
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Simplifier_clicked()
+{
+
+}
+
+
+void SaisieExpressionGraphe::on_Bouton_Carre_clicked()
+{
+
+}
+
+
+void SaisieExpressionGraphe::on_SpinBox_Constante_valueChanged(double arg1)
+{
+    _nombreConstanteSaisie = arg1;
+}
+
--- saisieexpressiongraphe.h
+++ saisieexpressiongraphe.h
@@ -1,6 +1,35 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QMessageBox>
+
+#include <QApplication>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLayout>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stack>
+
+#include "graphe.h"
+#include "Expression.h"
+#include "Constante.h"
+#include "Multiplication.h"
+#include "Addition.h"
+#include "Soustraction.h"
+#include "Division.h"
+#include "Constante.h"
+#include "Variable.h"
+
+#include "fenetregraphe.h"
 
 namespace Ui {
 class SaisieExpressionGraphe;
@@ -14,7 +43,36 @@ public:
     explicit SaisieExpressionGraphe(QWidget *parent = nullptr);
     ~SaisieExpressionGraphe();
 
+private slots:
+    void on_Bouton_Constante_clicked();
+
+    void on_Bouton_Variable_clicked();
+
+
+    void on_Bouton_Addition_clicked();
+
+    void on_Bouton_Multiplication_clicked();
+
+    void on_Bouton_Puissance_clicked();
+
+    void on_Bouton_Soustraction_clicked();
+
+    void on_Bouton_Inverse_clicked();
+
+    void on_Bouton_Division_clicked();
+
+    void on_Bouton_ValAbs_clicked();
+
+    void on_Bouton_Simplifier_clicked();
+
+    void on_Bouton_Carre_clicked();
+
+    void on_SpinBox_Constante_valueChanged(double arg1);
+
 private:
     Ui::SaisieExpressionGraphe *ui;
+    std::stack<Expression*> pileExpression;
+    double _nombreConstanteSaisie;
+
 };
 
