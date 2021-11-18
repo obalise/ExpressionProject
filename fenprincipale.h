d9b2fea48d6d2746d8a0a0320a12b83bd117cc26
activiteOCR <julien.terrier3@wanadoo.fr>
2021-11-18 19:57:42 +0100

    conflit

---
+++ fenprincipale.h
@@ -0,0 +1,57 @@
+#ifndef FENPRINCIPALE_H
+#define FENPRINCIPALE_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include <QList>
+#include "Addition.h"
+
+#include "Expression.h"
+#include "saisieexpressionIHM.h"
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QMenu;
+class QTextEdit;
+QT_END_NAMESPACE
+
+class FenPrincipale : public QMainWindow
+{
+    Q_OBJECT
+    public:
+    //explicit FenPrincipale(QWidget *parent = nullptr);
+    FenPrincipale(int x, int y);
+    Expression* getExpression();
+    void setExpression(Expression*);
+    SaisieExpressionIHM *saisieExpressionGraphique = 0; //Nous aurons besoin de ce pointeur pour créer notre fenêtre de Saisie de l'expression
+
+    private:
+    QString *fileName;
+    QTextEdit *textEdit;
+    QString mot;
+    QString texte;
+    QString curFile;
+    QLabel* imageLabel;
+<<<<<<< HEAD
+    Addition* myAdd;
+=======
+    Expression* _monExpression;
+>>>>>>> Florent
+
+    public slots:
+    bool sauvegarder();
+    bool saveFile(const QString &fileName);
+    void charger();
+    void saisie();
+    void affichageNC();
+    void affichageNPI();
+    void affichageVal();
+    void affichageGraph();
+    void affichageSimplification();
+
+
+    signals:
+
+};
+
+#endif // FENPRINCIPALE_H
