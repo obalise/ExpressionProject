f4d04ff5178c6438243f61650f7e93aceee63ecd
fred-isen <74205795+fred-isen@users.noreply.github.com>
2021-11-20 11:40:10 +0100

    Mise a jour des fonctions de Simplification

--- Addition.cpp
+++ Addition.cpp
@@ -49,44 +49,126 @@ float Addition::calculer()
 
 Expression* Addition::simplifier()
 {
-    Expression *temp;
-    Constante add=0;
-        if ((typeid(*_operandeGauche) == typeid(Variable)) || (typeid(*_operandeDroite) == typeid(Variable)))
+    Constante *New_cte; // Creation d'un pointeur de type Constante
+    Expression *New_exp; // Creation d'un pointeur de type Expression
+    float result=0;
+
+        if (typeid(*_operandeGauche) == typeid(Variable)) // cas de l'operande_Gauche = Variable
             {
-                //cout<< "OpD ou OpD=Var "<<endl;
-                return this;
-            }
-            else if ((typeid(*_operandeGauche) == typeid(Constante)) && (typeid(*_operandeDroite) == typeid(Constante)))
+                if (typeid(*_operandeDroite) == typeid(Variable))
+                    {
+                        //cout << "var & var "<< *this<< endl; // Pour debug
+                        return this; // On renvoi l'expression non simplifiable car contient 2 variables
+                    }
+                else if (typeid(*_operandeDroite) == typeid(Constante))
+                    {
+                        //cout << "var & cte "<< *this<< endl; // Pour debug
+                        return this; // On renvoi l'expression non simplifiable car contient 1 variable et 1 constante
+                    }
+                else // cas de l'operande_Droite = Expression
                 {
-                    //cout<< "OpG & D=Cte :"<<endl;
-                    Addition res2(_operandeGauche,_operandeDroite);
-                    add=res2.calculer();
+                    _operandeDroite=_operandeDroite->simplifier(); // Simplification de l'opérande droite dans le cas d'une expression
+                    Addition add_res(_operandeGauche,_operandeDroite); // Recreation d'une nouvelle addition avec operande droite simplifiee
+                    New_exp=new Addition(add_res); // Affectation au pointeur de la nouvelle expression pour recuperation par le main
+                    //cout << "var & exp "<< *New_exp<< endl; // Pour debug
+                    return New_exp;
                 }
-                else if ((typeid(*_operandeGauche) == typeid(Constante)) && (typeid(*_operandeDroite) != typeid(Constante)))
+            }
+
+        else if (typeid(*_operandeGauche) == typeid(Constante))
+            {
+                if (typeid(*_operandeDroite) == typeid(Variable))
                     {
-                        //cout<< "OpG=Cte & OPD=Exp :"<<endl;
-                        Addition res2(_operandeGauche,_operandeDroite->simplifier());
-                        add=res2.calculer();
+                        // cout << "Cte & var "<< *this<< endl; // Pour debug
+                        return this; // On renvoi l'expression non simplifiable car contient 2 variables
                     }
-                    else if ((typeid(*_operandeGauche) != typeid(Constante)) && (typeid(*_operandeDroite) == typeid(Constante)))
+                else if (typeid(*_operandeDroite) == typeid(Constante))
                     {
-                        //cout<< "OpG=Exp & OpD=Cte :"<<endl;
-                        Addition res2(_operandeGauche->simplifier(),_operandeDroite);
-                        add=res2.calculer();
+                    Addition add_res(_operandeGauche,_operandeDroite);
+                    result=add_res.calculer();
+                    New_cte=new Constante(result);
+                    //cout << "cte & cte "<< *New_cte << endl; // Pour debug
+                    return New_cte;
                     }
-                        else
+                else // cas de l'operande_Droite = expression quand operande_Gauche = constante
+                {
+                    _operandeDroite=_operandeDroite->simplifier();
+                    Addition add_res(_operandeGauche,_operandeDroite);
+                    if (typeid(*_operandeDroite) != typeid(Constante))
+                        {
+                            //cout << "cte & exp apres simplification "<< *this<< endl; // Pour debug
+                            return this;
+                        }
+                    else
+                        {
+                            result=add_res.calculer();
+                            New_cte=new Constante(result);
+                            //cout << "cte & cte apres simplification "<< *New_cte<< endl; // Pour debug
+                            return New_cte;
+                        }
+                }
+            }
+        else // cas de l'operande_Gauche = Expression
+        {
+            _operandeGauche=_operandeGauche->simplifier();
+            Addition add_res(_operandeGauche,_operandeDroite);
+            if (typeid(*_operandeGauche) != typeid(Constante))
+                {
+                    if (typeid(*_operandeDroite) == typeid(Variable))
+                        {
+                            //cout << "exp apres simplification & var "<< *this << endl; // Pour debug
+                            return this;
+                        }
+                    else if (typeid(*_operandeDroite) == typeid(Constante))
+                        {
+                            New_exp=new Addition(add_res);
+                            //cout << "exp apres simplification & cte "<< *New_exp << endl; // Pour debug
+                            return New_exp;
+                        }
+                    else // quand operande_droite = Expression
+                        {
+                            _operandeDroite=_operandeDroite->simplifier();
+                            Addition add_res(_operandeGauche,_operandeDroite);
+                            New_exp=new Addition(add_res);
+                            //cout << "exp apres simplification & exp apres simplification "<< *New_exp << endl; // Pour debug
+                            return New_exp;
+                        }
+                }
+            else // cas de l'operande_Gauche apres simplification = constante
+                {
+                    if (typeid(*_operandeDroite) == typeid(Variable))
+                        {
+                            //cout << "cte apres simplification & var "<< *this<< endl; // Pour debug
+                            return this;
+                        }
+                    else if (typeid(*_operandeDroite) == typeid(Constante))
+                        {
+                            result=add_res.calculer();
+                            New_cte=new Constante(result);
+                            //cout << "cte apres simplification & cte "<< *New_cte<< endl; // Pour debug
+                            return New_cte;
+                        }
+                    else // cas de l'operande_Droite = Expression
+                        {
+                            _operandeDroite=_operandeDroite->simplifier();
+                            Addition add_res(_operandeGauche,_operandeDroite);
+                            if (typeid(*_operandeDroite) != typeid(Constante))
+                                {
+                                    New_exp=new Addition(add_res);
+                                    //cout << "cte apres simplification & exp apres simplification "<< *New_exp<< endl; // Pour debug
+                                    return New_exp;
+                                }
+                            else // Cas de l'operande_Droite apres simplification = Constante
                             {
-                                //cout<< "OpG=Exp & OPD=Exp :"<<endl;
-                                Addition res2(_operandeGauche->simplifier(),_operandeDroite->simplifier());
-                                add=res2.calculer();
+                                result=add_res.calculer();
+                                New_cte=new Constante(result);
+                                //cout << "cte apres simplification & cte apres simplification "<< *New_cte<< endl; // Pour debug
+                                return New_cte;
                             }
+                        }
+                }
+        }
 
-    temp=&add;
-
-    cout<< "Resultat fct add simplifier final="<<add<<endl;
-    cout<< "Resultat fct addr Tempo add simplifier final="<<&temp<<endl;
-    cout<< "Resultat fct contenu Tempo add simplifier final="<<temp<<endl;
-    return this;
 }
 
 ostream &operator<<(ostream &os, const Addition& op)
